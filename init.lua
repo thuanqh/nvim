e6408a76c32ee59b812f7485cd27cbda73ec5f7a
Jason Quach <thuanqh@gmail.com>
2022-10-21 07:24:45 +0700

    Init basic IDE config

---
+++ init.lua
@@ -0,0 +1,26 @@
+require "user.plugins"
+if vim.g.vscode then
+  -- VSCode extension
+else
+require "user.options"
+require "user.keymaps"
+require "user.autocommands"
+require "user.colorscheme"
+require "user.cmp"
+require "user.telescope"
+require "user.gitsigns"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.impatient"
+require "user.illuminate"
+require "user.indentline"
+require "user.alpha"
+require "user.lsp"
+require "user.dap"
+end
